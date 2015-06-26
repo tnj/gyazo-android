776741213990b4dfb6830a792e180ce363ee6ca3
tnj <kitakore@gmail.com>
2015-06-26 14:06:19 +0900

    upload image to gyazo

---
+++ app/src/main/java/sh/nothing/gyazo/LauncherActivity.java
@@ -0,0 +1,99 @@
+package sh.nothing.gyazo;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import java.io.File;
+
+import sh.nothing.gyazo.service.ImageUploadService;
+
+public class LauncherActivity extends Activity {
+
+    private static final String SCREENSHOTS_DIR_NAME = "Screenshots";
+    private static final long RECENT_THREASHOLD_MILLIS = 60000L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Uri target = findUploadableScreenshot();
+        if (target != null) {
+            uploadScreenshot(target);
+        } else {
+            startPreference();
+        }
+
+        finish();
+    }
+
+    private void startPreference() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private void uploadScreenshot(Uri target) {
+        // TODO implement
+        Toast.makeText(this, target.toString(), Toast.LENGTH_LONG).show();;
+
+        Intent i = new Intent(this, ImageUploadService.class);
+        i.setData(target);
+        startService(i);
+    }
+
+    private Uri findUploadableScreenshot() {
+        Uri target = extractTargetFromIntent();
+        if (target == null) {
+            target = findTargetFromMediaStore();
+        }
+        return target;
+    }
+
+    private Uri findTargetFromMediaStore() {
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[]{
+                        MediaStore.Images.ImageColumns._ID,
+                        MediaStore.Images.ImageColumns.DATE_TAKEN
+                },
+                MediaStore.Images.ImageColumns.MIME_TYPE + "=? AND " +
+                        MediaStore.Images.ImageColumns.DATA + " LIKE ?",
+                new String[]{
+                        "image/png",
+                        getScreenshotDir() + "%"
+                },
+                MediaStore.Images.ImageColumns._ID + " DESC");
+
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    if (System.currentTimeMillis() - RECENT_THREASHOLD_MILLIS < cursor.getLong(1))
+                        return ContentUris.withAppendedId(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                cursor.getLong(0));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+
+    @NonNull
+    private String getScreenshotDir() {
+        File screenShotDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), SCREENSHOTS_DIR_NAME);
+        return screenShotDir.getAbsolutePath();
+    }
+
+    private Uri extractTargetFromIntent() {
+        // TODO implement
+        return null;
+    }
+}
---
+++ app/src/main/java/sh/nothing/gyazo/service/ImageUploadService.java
@@ -0,0 +1,140 @@
+
+package sh.nothing.gyazo.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.MultipartBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.File;
+import java.io.IOException;
+
+import sh.nothing.gyazo.R;
+
+public class ImageUploadService extends IntentService {
+
+    private static final String TAG = "ImageUploadService";
+    public static final String PREFS_GYAZO_ID = "id";
+
+    public ImageUploadService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Uri uri = intent.getData();
+
+        onStart();
+        String result = null;
+        try {
+            result = upload(uri);
+        } catch (IOException e) {
+            Log.w(TAG, "Failed to upload an image", e);
+        }
+
+        if (result == null) {
+            onFailed();
+        } else {
+            onSucceed(result);
+        }
+    }
+
+    private void onSucceed(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    private void onFailed() {
+        Toast.makeText(this, R.string.failed_to_upload_image, Toast.LENGTH_LONG).show();
+    }
+
+    private void onStart() {
+        Toast.makeText(this, R.string.uploading_image, Toast.LENGTH_SHORT).show();
+    }
+
+    private String upload(final Uri uri) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        String id = loadGyazoId();
+        String type = getContentResolver().getType(uri);
+        File file = getFileFromContentUri(uri);
+
+        RequestBody body = new MultipartBuilder()
+                .type(MultipartBuilder.FORM)
+                .addPart(
+                        Headers.of("Content-Disposition", "form-data; name=\"id\""),
+                        RequestBody.create(null, id)
+                )
+                .addPart(
+                        Headers.of("Content-Disposition", "form-data; name=\"imagedata\"; filename=\"gyazo.com\""),
+                        RequestBody.create(MediaType.parse(type), file)
+                )
+                .build();
+
+        Request request = new Request.Builder()
+                .url(getEndpointUrl())
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            String url = response.body().string();
+
+            id = response.header("X-Gyazo-Id");
+            if (!TextUtils.isEmpty(id))
+                saveGyazoId(id);
+
+            return url;
+        }
+        return null;
+    }
+
+    private SharedPreferences getPrefs() {
+        return PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    private String loadGyazoId() {
+        return getPrefs().getString(PREFS_GYAZO_ID, "");
+    }
+
+    private void saveGyazoId(String id) {
+        getPrefs().edit().putString(PREFS_GYAZO_ID, id).apply();
+    }
+
+    public String getEndpointUrl() {
+        return "http://upload.gyazo.com/upload.cgi";
+    }
+
+    public File getFileFromContentUri(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri,
+                new String[]{
+                        MediaStore.Images.ImageColumns.DATA,
+                }, null, null, null);
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    return new File(cursor.getString(0));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+}
